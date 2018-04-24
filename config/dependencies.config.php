b7b46b51e1254fa7d51a8356f0636cddb82d1703
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2018-04-24 23:49:54 +0100

    added omited Zfe namespace prefix

--- config/dependencies.config.php
+++ config/dependencies.config.php
@@ -2,7 +2,7 @@
 
 namespace Zfe\Common;
 
-use Common\Factory\LoggingErrorListenerFactory;
+use Zfe\Common\Factory\LoggingErrorListenerFactory;
 use Zend\Stratigility\Middleware\ErrorHandler;
 
 return [
